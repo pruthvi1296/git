68f6dfb92ef2128b2576fb8abcc990a439ecbb68
pruthvi1296 <darekarpruthvi@gmail.com>
2025-02-22 13:54:31 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,20 @@
+function updateclock(){
+    const now=new Date();
+    const hours=now.getHours().toString().padStart(2,0);
+    const minutes=now.getMinutes().toString().padStart(2,0);
+    const seconds=now.getSeconds().toString().padStart(2,0);
+    
+    const date=now.getDate().toString().padStart(2,0);
+    const month=now.getMonth().toString().padStart(2,0);
+    const year=now.getFullYear().toString().padStart(2,0);
+    
+    const timestring=`${now}`;
+    
+    document.getElementById("clock").textContent=timestring;
+   
+
+
+
+}
+updateclock();
+setInterval(updateclock,10);
